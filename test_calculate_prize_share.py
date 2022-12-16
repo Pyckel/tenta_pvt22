eabc3d06b4602e8e15c069d58f821ff56f6f5a54
Simon <spamalot@live.se>
2022-12-16 11:45:46 +0100

    ändrade så att bara calculate_prize_share importeras och inte hela nobel_prize.py

--- test_calculate_prize_share.py
+++ test_calculate_prize_share.py
@@ -1,5 +1,5 @@
 import pytest
-import nobel_prize
+from nobel_prize import calculate_prize_share
 
 
 test_data = [('100', '1', 100), ('100', '1/3', 33.333), ('1000', '1/2', 500)]
@@ -7,4 +7,4 @@ test_data = [('100', '1', 100), ('100', '1/3', 33.333), ('1000', '1/2', 500)]
 
 @pytest.mark.parametrize('total_pris, andel, resultat', test_data)
 def test_calculate_prize_share(total_pris, andel, resultat):
-    assert nobel_prize.calculate_prize_share(total_pris, andel) == resultat
+    assert calculate_prize_share(total_pris, andel) == resultat
